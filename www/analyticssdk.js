c7c81c16e429f54b18008ec21079fd5f735928a6
MrPi <pimingqiang@vip.qq.com>
2020-07-02 17:48:23 +0800

    更新格式

--- www/analyticssdk.js
+++ www/analyticssdk.js
@@ -7,8 +7,8 @@ var MobclickAgent = {
      * @param eventId
      *            String类型.事件ID，注意需要先在友盟网站注册此ID
      */
-    onEvent : function(eventId) {
-        cordova.exec(null, null, "AnalyticsSDK","onEvent", [ eventId ]);
+    onEvent: function (eventId) {
+        cordova.exec(null, null, "AnalyticsSDK", "onEvent", [eventId]);
 
     },
     /**
@@ -19,8 +19,8 @@ var MobclickAgent = {
      * @param eventLabel
      *            String类型.事件标签，事件的一个属性说明
      */
-    onEventWithLabel : function(eventId, eventLabel) {
-        cordova.exec(null, null, "AnalyticsSDK","onEventWithLabel", [ eventId, eventLabel ]);
+    onEventWithLabel: function (eventId, eventLabel) {
+        cordova.exec(null, null, "AnalyticsSDK", "onEventWithLabel", [eventId, eventLabel]);
 
     },
     /**
@@ -31,8 +31,8 @@ var MobclickAgent = {
      * @param eventData
      *            Dictionary类型.当前事件的属性集合，最多支持10个K-V值
      */
-    onEventWithParameters : function(eventId, eventData) {
-        cordova.exec(null, null, "AnalyticsSDK","onEventWithParameters", [ eventId, eventData ]);
+    onEventWithParameters: function (eventId, eventData) {
+        cordova.exec(null, null, "AnalyticsSDK", "onEventWithParameters", [eventId, eventData]);
 
     },
     /**
@@ -46,8 +46,8 @@ var MobclickAgent = {
      *            int 类型.事件持续时长，单位毫秒，您需要手动计算并传入时长，作为事件的时长参数
      *
      */
-    onEventWithCounter : function(eventId, eventData, eventNum) {
-        cordova.exec(null, null, "AnalyticsSDK","onEventWithCounter", [ eventId, eventData, eventNum ]);
+    onEventWithCounter: function (eventId, eventData, eventNum) {
+        cordova.exec(null, null, "AnalyticsSDK", "onEventWithCounter", [eventId, eventData, eventNum]);
 
     },
 
@@ -55,8 +55,8 @@ var MobclickAgent = {
     * 支持多种属性值类型的自定义带属性事件接口。
     * 属性值可以是如下几种类型之一：String、Long、Integer、Float、Double、Short。
     */
-    onEventObject : function(eventId, eventData) {
-        cordova.exec(null, null, "AnalyticsSDK","onEventObject", [ eventId, eventData ]);
+    onEventObject: function (eventId, eventData) {
+        cordova.exec(null, null, "AnalyticsSDK", "onEventObject", [eventId, eventData]);
     },
 
     /**
@@ -65,8 +65,8 @@ var MobclickAgent = {
      * @param pageName
      *            String类型.页面名称
      */
-    onPageBegin : function(pageName) {
-        cordova.exec(null, null, "AnalyticsSDK","onPageBegin", [ pageName ]);
+    onPageBegin: function (pageName) {
+        cordova.exec(null, null, "AnalyticsSDK", "onPageBegin", [pageName]);
 
     },
     /**
@@ -75,14 +75,14 @@ var MobclickAgent = {
      * @param pageName
      *            String类型.页面名称
      */
-    onPageEnd : function(pageName) {
-        cordova.exec(null, null, "AnalyticsSDK","onPageEnd", [ pageName ]);
+    onPageEnd: function (pageName) {
+        cordova.exec(null, null, "AnalyticsSDK", "onPageEnd", [pageName]);
 
     },
     /**
      * 获取IOS UUID
      */
-    getDeviceId:function(callBack) {
+    getDeviceId: function (callBack) {
         cordova.exec(callBack, null, "AnalyticsSDK", "getDeviceId", []);
 
     },
@@ -92,7 +92,7 @@ var MobclickAgent = {
      * @param UID
      *            用户账号ID,长度小于64字节
      */
-    profileSignInWithPUID:function(UID) {
+    profileSignInWithPUID: function (UID) {
         cordova.exec(null, null, "AnalyticsSDK", "profileSignInWithPUID", [UID]);
     },
     /**
@@ -104,60 +104,60 @@ var MobclickAgent = {
      * @param UID
      *            用户账号ID,长度小于64字节
      */
-    profileSignInWithPUIDWithProvider:function(provider, UID) {
-        cordova.exec(null, null, "AnalyticsSDK", "profileSignInWithPUIDWithProvider", [ provider, UID ]);
+    profileSignInWithPUIDWithProvider: function (provider, UID) {
+        cordova.exec(null, null, "AnalyticsSDK", "profileSignInWithPUIDWithProvider", [provider, UID]);
 
     },
     /**
      * 帐号统计退出接口
      */
-    profileSignOff:function(){
+    profileSignOff: function () {
         cordova.exec(null, null, "AnalyticsSDK", "profileSignOff", []);
 
     },
-     /** * 设置属性 键值对 会覆盖同名的key
-      * 将该函数指定的key-value写入dplus专用文件；APP启动时会自动读取该文件的所有key-value，并将key-value自动作为后续所有track事件的属性。
-      * @param property
-      *              Dictionary类型.自定义属性
-      *
-      */
-      registerSuperProperty : function(property) {
-         cordova.exec(null, null, "AnalyticsSDK","registerSuperProperty", [property]);
-      },
+    /** * 设置属性 键值对 会覆盖同名的key
+     * 将该函数指定的key-value写入dplus专用文件；APP启动时会自动读取该文件的所有key-value，并将key-value自动作为后续所有track事件的属性。
+     * @param property
+     *              Dictionary类型.自定义属性
+     *
+     */
+    registerSuperProperty: function (property) {
+        cordova.exec(null, null, "AnalyticsSDK", "registerSuperProperty", [property]);
+    },
 
-     /** * 从dplus专用文件中删除指定key-value
-      * @param propertyName
-      *              String类型.自定义属性
-      *
-      */
-      unregisterSuperProperty : function(propertyName) {
-          cordova.exec(null, null, "AnalyticsSDK","unregisterSuperProperty", [propertyName]);
-       },
+    /** * 从dplus专用文件中删除指定key-value
+     * @param propertyName
+     *              String类型.自定义属性
+     *
+     */
+    unregisterSuperProperty: function (propertyName) {
+        cordova.exec(null, null, "AnalyticsSDK", "unregisterSuperProperty", [propertyName]);
+    },
 
-       /** 返回所有key-value；如果不存在，则返回空。
-        *
-        */
-        getPreProperties : function(callBack) {
-               cordova.exec(callBack, null, "AnalyticsSDK","getPreProperties", []);
-        },
+    /** 返回所有key-value；如果不存在，则返回空。
+     *
+     */
+    getPreProperties: function (callBack) {
+        cordova.exec(callBack, null, "AnalyticsSDK", "getPreProperties", []);
+    },
 
-        /** * 清空Dplus专用文件中的所有key-value。
-         *
-         */
-         clearPreProperties : function() {
-               cordova.exec(null, null, "AnalyticsSDK","clearPreProperties", []);
-         },
+    /** * 清空Dplus专用文件中的所有key-value。
+     *
+     */
+    clearPreProperties: function () {
+        cordova.exec(null, null, "AnalyticsSDK", "clearPreProperties", []);
+    },
 
-        /** * 设置关注事件是否首次触发,只关注eventList前五个合法eventID.只要已经保存五个,此接口无效
-         * @param eventList
-         *              Array类型.自定义属性
-         *
-         */
-         setFirstLaunchEvent : function(eventList) {
-               cordova.exec(null, null, "AnalyticsSDK","setFirstLaunchEvent", [eventList]);
-         }
+    /** * 设置关注事件是否首次触发,只关注eventList前五个合法eventID.只要已经保存五个,此接口无效
+     * @param eventList
+     *              Array类型.自定义属性
+     *
+     */
+    setFirstLaunchEvent: function (eventList) {
+        cordova.exec(null, null, "AnalyticsSDK", "setFirstLaunchEvent", [eventList]);
+    }
 };
 
-     module.exports =MobclickAgent;
+module.exports = MobclickAgent;
 
 
